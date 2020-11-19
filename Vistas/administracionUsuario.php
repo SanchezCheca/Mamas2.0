c59569710b81b20f3dcabc44d9b6630c393f7cae
nestor <nestor1996sanchez@gmail.com>
2020-11-19 19:02:52 +0100

    HTML con estilos del crud

--- Vistas/administracionUsuario.php
+++ Vistas/administracionUsuario.php
@@ -1,17 +1,145 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+
+
+<html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+
+        <title>Página principal</title>
+        <!-- MDB icon -->
+        <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+        <!-- Google Fonts Roboto -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <!-- Material Design Bootstrap -->
+        <link rel="stylesheet" href="../css/mdb.min.css">
+        <!-- Your custom styles (optional) -->
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../css/miestilo.css">
     </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
+    <body style="height: 100%;">
+
+
+        <!-- Navigation -->
+        <header class="row align-items-center navbar fixed-top navbar-expand-lg navbar-light white scrolling-navbar bg-primary">
+        
+    <!-- Navbar -->
+    
+      <div class="container">
+    <div class="col-md-6">
+        <!-- Brand -->
+      
+          <h2 class="font-weight-bold">Administración de Usuarios</h2>
+    </div>
+<div class="col-md-6">
+        <!-- Collapse -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Links -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+          <!-- Left -->
+          <ul class="navbar-nav mr-auto  offset-7">
+            <li class="nav-item ">
+                <form action="../Controladores/controladorCRUD.php" method="POST" name="volver">
+                    <button type="submit" class=" waves-effect  btn btn-primary" name="cerrarSesion" >Cerrar Sesión</button>
+                <form>
+            </li>
+           
+          </ul>
+ 
+
+        </div>
+
+      </div>
+   
+  
+            </div>
+
+  </header>
+
+
+
+        <main class="row ">
+
+            <div class="col-md-8 mx-auto mt-5 vh-80 d-flex flex-column justify-content-center align-items-center">
+                 <button type="button" class="btn btn-rounded btn-primary mb-5 fixed-top  marginmio"><i class="fas fa-user-plus pr-2" style="font-size: 20px" aria-hidden="true"></i>Añadir Alumno</button>
+                <div class="table-responsive text-nowrap ">
+                    <table class="table ">
+                        <thead>
+                            <tr>
+                                <th scope="col"> <i class="fas fa-id-card pr-2" style="font-size: 30px"></i>  Id</th>
+                                <th scope="col"><i class="fas fa-user-graduate pr-2" style="font-size: 30px" ></i>Nombre</th>
+                                <th scope="col"> <i class="fas fa-envelope-open-text pr-2" style="font-size: 30px"></i>Email</th>
+                                <th scope="col"><i class="fas fa-dice pr-2" style="font-size: 30px"></i>Rol</th>
+                                <th scope="col"><i class="fas fa-check pr-2" style="font-size: 30px"></i>Activado</th>
+                                <th scope="col"><i class="fas fa-user pr-2" style="font-size: 30px"></i>Editar/Eliminar</th>
+
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Cell</td>
+                                <td>Cell</td>
+                                <td>Cell</td>
+                                <td>Cell</td>
+                                <td>
+                                     <a type="button" class="btn-floating btn-lg  boton bg-primary"><i class="fas fa-user-edit"></i></a>
+                                      <a type="button" class="btn-floating btn-lg  boton bg-primary"><i class="fas fa-user-times"></i></a>
+                                </td>
+                                 
+                            </tr>
+                        
+                           
+                        </tbody>
+                    </table>
+
+
+                </div>
+
+            </div>
+           
+            
+        </main>
+
+
+
+
+        <!-- Footer -->
+
+        <footer class=" row  bg-primary fixed-bottom vh-10 ">
+            <div class="container">
+                <!-- Copyright -->
+                <div class="footer-copyright text-center py-3">© 2020 Copyright:
+                    <a href="" class="text-light"> Daniel y Néstor</a>
+                </div>
+                <!-- Copyright -->
+            </div>
+        </footer>
+        <!-- Footer -->
+
+
+        <!-- jQuery -->
+        <script type="text/javascript" src="../js/jquery.min.js"></script>
+        <!-- Bootstrap tooltips -->
+        <script type="text/javascript" src="../js/popper.min.js"></script>
+        <!-- Bootstrap core JavaScript -->
+        <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+        <!-- MDB core JavaScript -->
+        <script type="text/javascript" src="../js/mdb.min.js"></script>
+        <!-- Your custom scripts (optional) -->
+        <script type="text/javascript"></script>
+
+
     </body>
 </html>
---
+++ mamas2.sql
@@ -0,0 +1,239 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 18-11-2020 a las 13:32:45
+-- Versión del servidor: 5.7.32-0ubuntu0.18.04.1
+-- Versión de PHP: 7.2.24-0ubuntu0.18.04.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `mamas2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `aulas`
+--
+
+CREATE TABLE `aulas` (
+  `id` int(10) NOT NULL,
+  `idProfesor` int(10) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `aula_examen`
+--
+
+CREATE TABLE `aula_examen` (
+  `idAula` int(10) NOT NULL,
+  `idExamen` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `examenes`
+--
+
+CREATE TABLE `examenes` (
+  `id` int(10) NOT NULL,
+  `idProfesor` int(10) NOT NULL,
+  `fechaCreacion` date NOT NULL,
+  `abierto` int(1) NOT NULL,
+  `fechaInicio` date DEFAULT NULL,
+  `fechaFin` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `examen_pregunta`
+--
+
+CREATE TABLE `examen_pregunta` (
+  `idExamen` int(10) NOT NULL,
+  `idPregunta` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `opciones`
+--
+
+CREATE TABLE `opciones` (
+  `id` int(10) NOT NULL,
+  `idPregunta` int(10) NOT NULL,
+  `esCorrecta` int(1) NOT NULL,
+  `cuerpo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `preguntas`
+--
+
+CREATE TABLE `preguntas` (
+  `id` int(10) NOT NULL,
+  `cuerpo` varchar(100) NOT NULL,
+  `tipo` varchar(20) NOT NULL,
+  `valor` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `respuestas`
+--
+
+CREATE TABLE `respuestas` (
+  `id` int(10) NOT NULL,
+  `idPregunta` int(10) NOT NULL,
+  `idSolucion` int(10) NOT NULL,
+  `cuerpo` varchar(50) NOT NULL,
+  `puntuacion` int(2) DEFAULT NULL,
+  `comentario` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `soluciones`
+--
+
+CREATE TABLE `soluciones` (
+  `id` int(10) NOT NULL,
+  `idAlumno` int(10) NOT NULL,
+  `idExamen` int(10) NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `corregido` int(1) NOT NULL,
+  `nota` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(10) NOT NULL,
+  `rol` int(1) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `pass` varchar(100) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `activo` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `rol`, `correo`, `pass`, `nombre`, `activo`) VALUES
+(1, 0, 'daniel@daniel.com', '$1$vbO9DPzc$BvyUpqeW1bAynUbkQjq8d0', 'Daniel', 1),
+(4, 0, 'daniel2@daniel2.com', '$1$Mjro8HJw$wvQRoNrhgmanNtT0QhrOa.', 'Daniel2', 0),
+(5, 0, 'daniel3@daniel3.com', '$1$ItmYaHmO$LPfgl4vDKaOU/nxvBsd7r0', 'daniel3', 0);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `aulas`
+--
+ALTER TABLE `aulas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `examenes`
+--
+ALTER TABLE `examenes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `opciones`
+--
+ALTER TABLE `opciones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `preguntas`
+--
+ALTER TABLE `preguntas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `respuestas`
+--
+ALTER TABLE `respuestas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `soluciones`
+--
+ALTER TABLE `soluciones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `aulas`
+--
+ALTER TABLE `aulas`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `examenes`
+--
+ALTER TABLE `examenes`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `opciones`
+--
+ALTER TABLE `opciones`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `preguntas`
+--
+ALTER TABLE `preguntas`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `respuestas`
+--
+ALTER TABLE `respuestas`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `soluciones`
+--
+ALTER TABLE `soluciones`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
